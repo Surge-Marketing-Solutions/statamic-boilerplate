783e10aafb3825d16294e89b8e9297b05e8d0f13
SnowySurge <142220087+SnowySurge@users.noreply.github.com>
2026-03-31 10:53:15 +0100

    Adding in the updated robots.txt and llms.txt editor

--- app/Listeners/GlobalSetChanged.php
+++ app/Listeners/GlobalSetChanged.php
@@ -14,15 +14,34 @@ class GlobalSetChanged
      * @param  \Statamic\Events\GlobalVariablesSaving  $event
      * @return void
      */
+
+    public function __construct() {
+        \Log::info('This should be running');
+    }
+
     public function handle(GlobalVariablesSaving $event)
     {
+        \Log::info('Event should be firing.');
+
         if( $event->variables->robots_contents ) {
+            \Log::info('We have robots content to update!');
+            
             $content = str_replace("robots_contents: |-\n  ", "", $event->variables->robots_contents);
-            file_put_contents('./robots.txt', $content);
+            
+            \Log::info('Content to be updated - ' . strval($content));
+            
+            file_put_contents(public_path('./robots.txt'), $content);
+            
             \Log::info('We\'ve updated the robots.txt content! - ' . strval($content));
         } elseif( $event->variables->llms_content ) {
+            \Log::info('We have LLMS content to update!');
+            
             $content = str_replace("llms_content: |-\n  ", "", $event->variables->llms_content);
-            file_put_contents('./llms.txt', $content);
+            
+            \Log::info('Content to be updated - ' . strval($content));
+
+            file_put_contents(public_path('./llms.txt'), $content);
+
             \Log::info('We\'ve updated the llms.txt content! - ' . strval($content));
         }
     }
